062c92936eb745957328621a13746740d551bb28
Balanjani <balanjani.chinnari@gmail.com>
2024-03-29 21:26:01 -0500

    Balanjani: Created a segue for two screens and created a viewcontroller for BreakfastVC

--- MunchMap/MunchMap/UserInformationVC.swift
+++ MunchMap/MunchMap/UserInformationVC.swift
@@ -1,36 +1,36 @@
-//
-//  UserInformationVC.swift
-//  MunchMap
-//
-//  Created by Tarun Yada  on 3/28/24.
-//
+    //
+    //  UserInformationVC.swift
+    //  MunchMap
+    //
+    //  Created by Tarun Yada  on 3/28/24.
+    //
 
-import UIKit
+    import UIKit
 
-class UserInformationVC: UIViewController {
+    class UserInformationVC: UIViewController {
 
-    @IBOutlet weak var AgeTF: UITextField!
-    @IBOutlet weak var HeightTF: UITextField!
-    
-    @IBOutlet weak var WeightTF: UITextField!
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        @IBOutlet weak var AgeTF: UITextField!
+        @IBOutlet weak var HeightTF: UITextField!
+        
+        @IBOutlet weak var WeightTF: UITextField!
+        
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Do any additional setup after loading the view.
+        }
+        
+       
+        /*
+        // MARK: - Navigation
 
-        // Do any additional setup after loading the view.
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // Get the new view controller using segue.destination.
+            // Pass the selected object to the new view controller.
+        }
+        */
+
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
