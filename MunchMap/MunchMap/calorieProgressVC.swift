471efb1f06fd2430d626b8eaf137b1b952864a08
Balanjani <balanjani.chinnari@gmail.com>
2024-04-12 20:08:59 -0500

    Commit 2:  upated slider functionality in calorieProgressVC

--- MunchMap/MunchMap/calorieProgressVC.swift
+++ MunchMap/MunchMap/calorieProgressVC.swift
@@ -26,9 +26,26 @@ class calorieProgressVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        EatenPV.progress = 0.0
+        RemainingPV.progress = 0.0
+        FatPV.progress = 0.0
+        CarbsPV.progress = 0.0
+        ProtienPV.progress = 0.0
+        
+        
 
         // Do any additional setup after loading the view.
     }
+    func updateProgress(value: Float) {
+        EatenPV.progress = value
+        RemainingPV.progress = value
+        FatPV.progress = value
+        CarbsPV.progress = value
+        ProtienPV.progress = value
+        
+        }
+    
     
 
     /*
